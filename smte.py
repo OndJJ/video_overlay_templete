3ff780fc1b4b53bab7945244cfa8cb58cc0c0d80
OndJJ <jcjk0302@gmail.com>
2021-10-31 20:48:28 +0900

    test commit

--- smte.py
+++ smte.py
@@ -2,40 +2,68 @@ import cv2
 import os
 import math
 
-# mp4 파일 frame 추출
-folder = 'theta_test'
+
+
+folder = 'origin'
 #os.mkdir(folder)
-# use opencv to do the job
-print(cv2.__version__)  # my version is 3.1.0
-vidcap = cv2.VideoCapture(
-    r'mounting_000\mounting_000.mp4')
+path = '01_20210908170650302'
+filePath = os.path.join(path, "01_20210908170650302.mp4")
+print(filePath)
 
-tscap = cv2.VideoCapture(
-    r'project.avi')
-count = 0
-while True:
-    success, image = vidcap.read()
-    if not success:
-        break
-    # save frame as JPEG file
-    cv2.imwrite(os.path.join(folder, "frame{:d}.jpg".format(count)), image)
-    count += 1
-print("{} images are extacted in {}.".format(count, folder))
+if os.path.isfile(filePath):	# 해당 파일이 있는지 확인
+    # 영상 객체(파일) 가져오기
+    cap = cv2.VideoCapture(filePath)
+else:
+    print("파일이 존재하지 않습니다.")  
 
+# # 프레임을 정수형으로 형 변환
+frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))	# 영상의 넓이(가로) 프레임
+frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))	# 영상의 높이(세로) 프레임
+ 
+frame_size = (frameWidth, frameHeight)
+print('frame_size={}'.format(frame_size))
 
-print('원본 영상 info : ','mounting_000\mounting_000.mp4')
-width = vidcap.get(cv2.CAP_PROP_FRAME_WIDTH) # 또는 cap.get(3)
-height = vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT) # 또는 cap.get(4)
-fps = vidcap.get(cv2.CAP_PROP_FPS) # 또는 cap.get(5)
-all = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
-print('프레임 너비: %d, 프레임 높이: %d, 초당 프레임 수: %d, 전체 프레임 수: %d' %(width, height, fps, all))
+# frameRate = 33
+
+# count = 0 
+# while True:
+#     # 한 장의 이미지(frame)를 가져오기
+#     # 영상 : 이미지(프레임)의 연속
+#     # 정상적으로 읽어왔는지 -> retval
+#     # 읽어온 프레임 -> frame
+#     retval, frame = cap.read()
+#     if not(retval):	# 프레임정보를 정상적으로 읽지 못하면
+#         break  # while문을 빠져나가기
+        
+#     cv2.imshow('frame', frame)	# 프레임 보여주기
+#     key = cv2.waitKey(frameRate)  # frameRate msec동안 한 프레임을 보여준다
+    
+#     # 키 입력을 받으면 키값을 key로 저장 -> esc == 27(아스키코드)
+#     if key == 27:
+#         break	# while문을 빠져나가기
+#     # save frame as JPEG file
+#     cv2.imwrite(os.path.join(folder, "frame{:d}.jpg".format(count)), frame)
+#     count += 1
+#     print("{} images are extacted in {}.".format(count, folder))
+        
+# if cap.isOpened():	# 영상 파일(카메라)이 정상적으로 열렸는지(초기화되었는지) 여부
+#     cap.release()	# 영상 파일(카메라) 사용을 종료
+    
+cv2.destroyAllWindows()
+
+print('원본 영상 info : ',"01_20210908170650302.mp4")
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH) # 또는 cap.get(3)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT) # 또는 cap.get(4)
+fps = cap.get(cv2.CAP_PROP_FPS) # 또는 cap.get(5)
+#all = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+print('프레임 너비: %d, 프레임 높이: %d, 초당 프레임 수: %d' %(width, height, fps))
 print('###############################')
 
 
-print('변환 영상 info : ','project.avi')
-width = tscap.get(cv2.CAP_PROP_FRAME_WIDTH) # 또는 cap.get(3)
-height = tscap.get(cv2.CAP_PROP_FRAME_HEIGHT) # 또는 cap.get(4)
-fps = tscap.get(cv2.CAP_PROP_FPS) # 또는 cap.get(5)
-all = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
-print('프레임 너비: %d, 프레임 높이: %d, 초당 프레임 수: %d, 전체 프레임 수: %d' %(width, height, fps,all))
-print('###############################')
+# print('변환 영상 info : ','project.avi')
+# width = tscap.get(cv2.CAP_PROP_FRAME_WIDTH) # 또는 cap.get(3)
+# height = tscap.get(cv2.CAP_PROP_FRAME_HEIGHT) # 또는 cap.get(4)
+# fps = tscap.get(cv2.CAP_PROP_FPS) # 또는 cap.get(5)
+# all = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
+# print('프레임 너비: %d, 프레임 높이: %d, 초당 프레임 수: %d, 전체 프레임 수: %d' %(width, height, fps,all))
+# print('###############################')
